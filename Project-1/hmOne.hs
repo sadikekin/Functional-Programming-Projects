29b11e64d2ba381bf66743baf4f2b129fa7869dd
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-02-22 09:03:15 +0300

    basic implementation of daysInMonth and leap. sundaysOne is iin process

--- Project-1/hmOne.hs
+++ Project-1/hmOne.hs
@@ -1,6 +1,5 @@
 
 dayOfWeek :: Integer -> Integer -> Integer -> Integer
-
 dayOfWeek y m d = returnValue
   where
     j :: Double
@@ -23,3 +22,24 @@ dayOfWeek y m d = returnValue
     tTwo = floor  (fromIntegral k / fromIntegral 4)
     tThree = floor (j / fromIntegral 4)
     returnValue = (d + tOne + k + tTwo + tThree + 5 * floor (j)) `mod` 7
+
+
+sundaysOne :: Integer -> Integer -> Integer
+sundaysOne start end = sundays' y m
+  where
+    sundays' :: Integer -> Integer -> Integer
+    sundays' y m
+      | y > end = y
+      | otherwise = if dayOfWeek y m 1 == 1 then rest + 1 else rest where
+        nextY = y
+        nextM = m
+        rest = sundays' y m  
+
+leap :: Integer -> Bool
+leap y = (y `mod` 4 == 0) && (y `mod` 100 /= 0) || (y `mod` 400 == 0)
+
+daysInMonth :: Integer -> Integer -> Integer
+daysInMonth  m y
+  | m == 2 = if leap(y) then 29 else 28
+  | m == 4 || m == 6 || m == 9 || m == 11 = 30
+  | otherwise = 31
