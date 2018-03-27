00a336de4a8259d277abba94dc32185b0e60928b
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-03-27 09:58:29 +0300

    deriving error solution. Plus allSameColors and cumCards implementations

--- Project-2/solution.hs
+++ Project-2/solution.hs
@@ -1,8 +1,13 @@
 data Color = Red | Black
+          deriving Eq
 data Suit = Clubs | Diamonds | Hearts | Spades
+          deriving Eq
 data Rank = Num Int | Jack | Queen | King | Ace
+          deriving Eq
 data Card = Card { suit :: Suit, rank :: Rank }
+          deriving Eq
 data Move = Draw | Discard Card
+          deriving Eq
 
 cardColor :: Card -> Color
 cardColor c = case suit c of
@@ -23,10 +28,27 @@ removeCard :: [Card] -> Card -> [Card]
 removeCard cs c = removeCardHelper cs c 0
   where
     -- The integer is for checking if we find element or not
-    removeCard :: [Card] -> Card -> Int -> [Card]
-    removeCard [] x 1 = []
-    removeCard [] x 0 = error "Not found"
-    removeCard (c':cs') x y
-      | y == 1    = c' : removeCard cs' x y
-      | c' == x   = removeCard cs' x 1
-      | otherwise = c' : removeCard cs' x y
+    removeCardHelper :: [Card] -> Card -> Int -> [Card]
+    removeCardHelper [] x 1 = []
+    removeCardHelper [] x 0 = error "Not found"
+    removeCardHelper (c':cs') x y
+      | y == 1    = c' : removeCardHelper cs' x y
+      | c' == x   = removeCardHelper cs' x 1
+      | otherwise = c' : removeCardHelper cs' x y
+
+
+allSameColors :: [Card] -> Bool
+allSameColors [] = True
+allSameColors (x:[]) = True
+allSameColors (x:xs:xss) = case (cardColor x) /= (cardColor xs) of
+  False           -> False
+  True            -> allSameColors (xs:xss)
+
+sumCards :: [Card] -> Int
+sumCards cs = sumCardsHelper cs 0
+    where
+      sumCardsHelper :: [Card] -> Int -> Int
+      sumCardsHelper [] n = n
+      sumCardsHelper (x:xs) n = sumCardsHelper xs (n + cardValue x)
+
+Score :: [Card] -> Int -> Int
