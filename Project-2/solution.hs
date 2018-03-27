2ca58f154c45be8b574c2896739ebfc9299b6b62
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-03-28 00:31:04 +0300

    added functions about IO

--- Project-2/solution.hs
+++ Project-2/solution.hs
@@ -1,12 +1,18 @@
+import Data.Char
+
 data Color = Red | Black
           deriving Eq
-data Suit = Clubs | Diamonds | Hearts | Spades
+data Suit  = Clubs | Diamonds | Hearts | Spades
           deriving Eq
-data Rank = Num Int | Jack | Queen | King | Ace
+data Rank  = Num Int | Jack | Queen | King | Ace
           deriving Eq
-data Card = Card { suit :: Suit, rank :: Rank }
+data Card  = Card { suit :: Suit, rank :: Rank }
           deriving Eq
-data Move = Draw | Discard Card
+data Move  = Draw | Discard Card
+          deriving Eq
+
+------ ???????
+data State = End | Ongoing
           deriving Eq
 
 cardColor :: Card -> Color
@@ -51,4 +57,54 @@ sumCards cs = sumCardsHelper cs 0
       sumCardsHelper [] n = n
       sumCardsHelper (x:xs) n = sumCardsHelper xs (n + cardValue x)
 
-Score :: [Card] -> Int -> Int
+score :: [Card] -> Int -> Int
+score c g
+  | c' > g      = preliminaryScoreGreater
+  | otherwise   = preliminaryScoreSmaller
+    where
+      c' = sumCards c
+      preliminaryScoreGreater = if allSameColors c then 3 * c' - g else (3 * c' - g) `div` 2
+      preliminaryScoreSmaller = if allSameColors c then g - c' else g - c' `div` 2
+
+-- runGame :: [Card] -> [Move] -> Int -> Int
+
+
+convertSuit :: Char -> Suit
+convertSuit 'd' = Diamonds
+convertSuit 'c' = Clubs
+convertSuit 's' = Spades
+convertSuit 'h' = Hearts
+convertSuit  x  = error "Wrong input"
+
+
+convertRank :: Char -> Rank
+convertRank x
+  | x == 'J' || x == 'j'  = Jack
+  | x == 'K' || x == 'k'  = King
+  | x == 'Q' || x == 'q'  = Queen
+  | x == '1'              = Ace
+  | x == 'T' || x == 't'  = Num 10
+  | otherwise             = Num (digitToInt x)
+
+
+convertCard :: Char -> Char -> Card
+convertCard s r =  Card (convertSuit s) (convertRank r)
+
+
+
+
+
+readCards :: IO([Card])
+readCards = do
+  cardList <- readCards' ([]::[Card])
+  return cardList
+    where
+      readCards' :: [Card] -> IO([Card])
+      readCards' cs = do
+        line <- getLine
+        if line == "."
+          then return cs
+          else do
+            let suit = head line
+            let rank = head (tail line)
+            readCards' ((convertCard suit rank) : cs)
