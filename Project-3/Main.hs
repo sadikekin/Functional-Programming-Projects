afd946af651319fced46c30bf123679054d99b1b
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-04-29 19:29:42 +0300

    function 7 implementations

--- Project-3/Main.hs
+++ Project-3/Main.hs
@@ -2,6 +2,7 @@ import Data.Map (Map, (!))
 import qualified Data.Map as M
 import qualified Data.List as L
 import Data.Char
+import Control.Monad.Fix
 
 data WordInput  = WordInput {contentWord :: [Char]}
           deriving (Eq, Show, Ord)
@@ -34,7 +35,7 @@ dictCharCounts wordList = dictCharCountsHelper $ contentSentence wordList
     dictCharCountsHelper [] = M.empty
     dictCharCountsHelper (wrdList:wrdList') = M.insert (wrdList) (wordCharCounts wrdList) $ dictCharCountsHelper wrdList'
 
-dictWordsByCharCounts :: M.Map WordInput CharacterCount -> M.Map (CharacterCount) [WordInput]
+dictWordsByCharCounts :: M.Map WordInput CharacterCount -> M.Map CharacterCount [WordInput]
 dictWordsByCharCounts fListMap = dictWordsByCharCountsSwapKeysWithValues (M.keys fListMap) (M.elems fListMap)
   where
     dictWordsByCharCountsSwapKeysWithValues [] []         = M.empty
@@ -43,3 +44,20 @@ dictWordsByCharCounts fListMap = dictWordsByCharCountsSwapKeysWithValues (M.keys
 
 wordAnagrams :: M.Map CharacterCount [WordInput] -> WordInput -> [WordInput]
 wordAnagrams fListMap w = fListMap ! (wordCharCounts w)
+
+
+subtractCounts :: CharacterCount -> CharacterCount -> CharacterCount
+subtractCounts ccOne ccTwo = CharacterCount { contentCC = subtractCountsHelper (contentCC ccOne) (flatter $ M.toList $ contentCC ccTwo) }
+  where
+    -- Flatter function evolves [('a',2)] to [('a',1),('a',1)]
+    flatter (x:xs) = if xs /= [] then flatterRecursive (fst x) (snd x) ++ flatter xs else flatterRecursive (fst x) (snd x)
+      where
+        flatterRecursive _  0 = []
+        flatterRecursive f s = (f, 1) : flatterRecursive f (s-1)
+    --------------------------------------------
+    removerFunc k e = if e > 1 then Just (e-1) else Nothing
+    --------------------------------------------
+    subtractCountsHelper ccOne'  (ccTwo':[])        = M.updateWithKey removerFunc (fst ccTwo') (ccOne')
+    subtractCountsHelper ccOne'  (ccTwo':ccTwos')   = subtractCountsHelper currentCCOne ccTwos'
+      where
+        currentCCOne = M.updateWithKey removerFunc (fst ccTwo') (ccOne')
