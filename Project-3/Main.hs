29869f025a29a07a88dd0e77e9f8d016a3c43987
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-05-01 14:00:40 +0300

    beautiful implementation

--- Project-3/Main.hs
+++ Project-3/Main.hs
@@ -5,81 +5,75 @@ import qualified Data.List as L
 import Data.Char
 import Control.Monad.Fix
 
-data WordInput  = WordInput {contentWord :: [Char]}
-          deriving (Eq, Show, Ord)
-data Sentence  = Sentence {contentSentence :: [WordInput]}
-          deriving (Eq, Show, Ord)
-data CharacterCount  = CharacterCount {contentCC :: M.Map Char Int}
-          deriving (Eq, Show, Ord)
+type WordInput  = [Char]
+type Sentence  = [WordInput]
+type CharacterCount  = M.Map Char Int
 
 
 wordCharCounts :: WordInput ->  CharacterCount
-wordCharCounts x = CharacterCount{ contentCC = wordCharCountsHelper (lowerChanger $ contentWord x) (L.nub $ lowerChanger $ contentWord x) }
+wordCharCounts x = wordCharCountsHelper (lowerChanger x) ((L.nub . lowerChanger) x)
   where
     lowerChanger (h:h')                             = if h' == [] then [h] else (toLower h) : (lowerChanger h')
     --------------------------------------------
     wordCharCountsHelper _ []                       = M.empty
-    wordCharCountsHelper originalList (elmnt:nList) =  M.insert elmnt (charNum originalList elmnt) $ wordCharCountsHelper originalList nList
+    wordCharCountsHelper originalList (elmnt:nList) = M.insert elmnt (charNum originalList elmnt) $ wordCharCountsHelper originalList nList
     --------------------------------------------
-    charNum s k                                     = length $ filter (== k) s
+    charNum s k                                     = length $ filter (\z -> z == k) s
 
 sentenceCharCounts :: Sentence -> CharacterCount
-sentenceCharCounts sentence                     = CharacterCount { contentCC =  sentenceCharCountsHelper $ contentSentence sentence }
+sentenceCharCounts sentence                     = sentenceCharCountsHelper sentence
   where
     sentenceCharCountsHelper []                 = M.empty
-    sentenceCharCountsHelper (wrdList:wrdList') = M.unionWith (+) (contentCC $ wordCharCounts wrdList) (sentenceCharCountsHelper wrdList')
+    sentenceCharCountsHelper (wrdList:wrdList') = M.unionWith (+) (wordCharCounts wrdList) (sentenceCharCountsHelper wrdList')
 
 
 dictCharCounts :: Sentence -> M.Map WordInput CharacterCount
-dictCharCounts wordList = dictCharCountsHelper $ contentSentence wordList
+dictCharCounts wordList                     = dictCharCountsHelper wordList
   where
-    dictCharCountsHelper [] = M.empty
+    dictCharCountsHelper []                 = M.empty
     dictCharCountsHelper (wrdList:wrdList') = M.insert (wrdList) (wordCharCounts wrdList) $ dictCharCountsHelper wrdList'
 
-dictWordsByCharCounts :: M.Map WordInput CharacterCount -> M.Map CharacterCount [WordInput]
-dictWordsByCharCounts fListMap = dictWordsByCharCountsSwapKeysWithValues (M.keys fListMap) (M.elems fListMap)
+dictWordsByCharCounts :: M.Map WordInput CharacterCount -> M.Map CharacterCount Sentence
+dictWordsByCharCounts fListMap                            = dictWordsByCharCountsSwapKeysWithValues (M.keys fListMap) (M.elems fListMap)
   where
     dictWordsByCharCountsSwapKeysWithValues [] []         = M.empty
     dictWordsByCharCountsSwapKeysWithValues (k:k') (e:e') = M.insertWith (++) (e) ([k]) $ dictWordsByCharCountsSwapKeysWithValues k' e'
 
 
-wordAnagrams :: M.Map CharacterCount [WordInput] -> WordInput -> [WordInput]
-wordAnagrams fListMap w = fListMap ! (wordCharCounts w)
+wordAnagrams :: M.Map CharacterCount Sentence -> WordInput -> Sentence
+wordAnagrams fListMap w                   = fListMap ! (wordCharCounts w)
 
--- charCountsSubsets :: CharacterCount -> [CharacterCount]
 charCountsSubsets :: CharacterCount -> [CharacterCount]
-charCountsSubsets cCount =  L.nub $ subSetArrayToMap $ charCountsSubsetsHelper (length flatterVersion) (flatterVersion)
+charCountsSubsets cCount                  = (L.nub . subSetArrayToMap) $ charCountsSubsetsHelper (length flatterVersion) (flatterVersion)
   where
-    flatterVersion = flatter $ M.toList $ contentCC cCount
+    flatterVersion                        = flatter ( M.toList cCount )
     --------------------------------------------
     -- Flatter function evolves [('a',2)] to [('a',1),('a',1)]
-    flatter (x:xs) = if xs /= [] then flatterRecursive (fst x) (snd x) ++ flatter xs else flatterRecursive (fst x) (snd x)
+    flatter (x:xs)                        = if xs /= [] then flatterRecursive (fst x) (snd x) ++ flatter xs else flatterRecursive (fst x) (snd x)
       where
-        flatterRecursive _  0 = []
-        flatterRecursive f s  = (f, 1) : flatterRecursive f (s-1)
+        flatterRecursive _  0             = []
+        flatterRecursive f s              = (f, 1) : flatterRecursive f (s-1)
     --------------------------------------------
     charCountsSubsetsHelper 0 _           = [[]]
-    charCountsSubsetsHelper number cCount = subSet number cCount ++ charCountsSubsetsHelper (number-1) cCount
-    --------------------------------------------
+    charCountsSubsetsHelper number cc = subSet number cc ++ charCountsSubsetsHelper (number-1) cc
+    -- --------------------------------------------
     -- Created all posible subsets for given array of tuples.
-    subSet number set = take (lenSet-number+1) (subSetHelper number set)
+    subSet number set                     = take (lenSet-number+1) (subSetHelper number set)
       where
         lenSet = length set
         --------------------------------------
-        subSetHelper _ []     = [[]]
-        subSetHelper num' (b:bs) = take num' (b:bs) : subSetHelper num' bs
-    --------------------------------------------
-    subSetArrayToMap []     = [CharacterCount { contentCC = M.empty }]
-    subSetArrayToMap (a:as) = CharacterCount { contentCC = subSetArrayToMapHelper a }  : subSetArrayToMap as
+        subSetHelper _ []                 = [[]]
+        subSetHelper num' (b:bs)          = take num' (b:bs) : subSetHelper num' bs
+    -- --------------------------------------------
+    subSetArrayToMap []                   = [M.empty]
+    subSetArrayToMap (a:as)               = subSetArrayToMapHelper a : subSetArrayToMap as
       where
-        subSetArrayToMapHelper []     = M.empty
-        subSetArrayToMapHelper (l:ls) = M.insertWith (+) (fst l) (snd l) (subSetArrayToMapHelper ls)
-
-
+        subSetArrayToMapHelper []         = M.empty
+        subSetArrayToMapHelper (l:ls)     = M.insertWith (+) (fst l) (snd l) (subSetArrayToMapHelper ls)
 
 
 subtractCounts :: CharacterCount -> CharacterCount -> CharacterCount
-subtractCounts ccOne ccTwo = CharacterCount { contentCC = subtractCountsHelper (contentCC ccOne) (flatter $ M.toList $ contentCC ccTwo) }
+subtractCounts ccOne ccTwo = subtractCountsHelper ccOne $ (flatter . M.toList) ccTwo
   where
     -- Flatter function evolves [('a',2)] to [('a',1),('a',1)]
     flatter (x:xs) = if xs /= [] then flatterRecursive (fst x) (snd x) ++ flatter xs else flatterRecursive (fst x) (snd x)
