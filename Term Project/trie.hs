e48f99271dace9beddc991a109e605588e15d625
Sadik Ekin Ozbay <ozbaysa@itu.edu.tr>
2018-05-11 08:30:27 +0300

    comments were added

--- Term Project/trie.hs
+++ Term Project/trie.hs
@@ -12,27 +12,31 @@ data Trie = Trie {end :: Bool, children :: M.Map Char Trie}
   deriving (Show, Eq)
 type Word = String
 
+-- Creates empty Trie
+empty ::  Trie
+empty = Trie {end = False, children = M.empty}
 
-empty :: Bool -> Trie
-empty withEndValue = Trie {end = withEndValue, children = M.empty}
-
+-- Inserts the element into given trie
 insert :: Word -> Trie -> Trie
 insert [] t                               = Trie {end = True, children = children t}
 insert (w:ws) t
-  | M.lookup w (children t) /= Nothing    = Trie { end = end t, children =  M.insert w (insert ws ( (children t) M.! w )) (children t) }
-  | otherwise                             = Trie { end = end t, children =  M.insert w (insert ws (( M.insert w (empty False) (children t) ) M.! w)) (children t) }
+  | M.lookup w (children t) /= Nothing    = Trie { end = end t, children =  M.insert w ( insert ws (((M.!) . children) t  w) ) (children t) }
+  | otherwise                             = Trie { end = end t, children =  M.insert w ( insert ws (( M.insert w empty (children t) ) M.! w) ) (children t) }
 
+-- Inserts all words one by one into given trie
 insertList :: [Word] -> Trie
-insertList w = foldr insert (empty False) w
+insertList w = foldr insert empty w
 
+-- Searches the given word from the dictionary
 search :: Word -> Trie -> Bool
 search (w:[]) t = if M.lookup w (children t) == Nothing then False else  end $ (children t) M.! w
 search (w:ws) t = if M.lookup w (children t) == Nothing then False else search ws ((children t) M.! w)
 
+-- Get all words from the trie
 getWords :: Trie -> [Word]
 getWords t = getWordsHelper t  "" []
   where
-    getWordsHelper :: Trie -> Word -> [Word] -> [Word]
+
     getWordsHelper t' currentWord wList
       | M.null $ children t'    = wList
       | otherwise               = childrenIterator (M.toList $ children t') currentWord wList
@@ -44,10 +48,11 @@ getWords t = getWordsHelper t  "" []
         where
           nodeWord              = currentWord ++ [(fst x)]
 
+-- This function finds all words in the dictionary and checks if the words has prefix of given word
 prefix :: Word -> Trie -> Maybe [Word]
 prefix w t =  if output == [] then Nothing else Just output
    where
-    output = [ x | x <- (reverse $ L.nub $ getWords t), (take (length w) x) == w]
+    output = [ x | x <- (reverse . L.nub) $ getWords t, (take . length) w x == w]
 
 
 takeInputsFromUser :: Trie -> IO()
@@ -56,7 +61,7 @@ takeInputsFromUser dictTrie = do
   action <- getLine
 
   if action == "a" then do
-
+    -- If action is "add", insert the element, which user was typed, to the dictionary
     putStrLn "Please enter the word: "
     wordUser <- getLine
     let newTrie = insert wordUser dictTrie
@@ -64,15 +69,15 @@ takeInputsFromUser dictTrie = do
     takeInputsFromUser newTrie
 
   else if action == "s" then do
-
-    putStrLn "Please enter a word: " -- Here we should search for prefix as well.
+    -- If action is "search", according to function result print outcome.
+    putStrLn "Please enter a word: "
     wordUser <- getLine
     if search wordUser dictTrie then do putStrLn "Exists in dictionary!" else do putStrLn "Does not exist in dictionary!"
     putStrLn "I am ready for your next instruction!"
     takeInputsFromUser dictTrie
 
   else if action == "f" then do
-
+    -- If action is "find", according to function result print outcome.
     putStrLn "Please enter a word/prefix: "
     pre <- getLine
     let foundPrefixes = fromMaybe [] (prefix pre dictTrie)
@@ -80,11 +85,12 @@ takeInputsFromUser dictTrie = do
       putStrLn "No words found with that prefix!"
     else do
       putStrLn "Found words"
-      putStrLn $ concat $ L.intersperse "\n" (foundPrefixes)
+      (putStrLn . concat) $ L.intersperse "\n" (foundPrefixes)
       putStrLn "I am ready for your next instruction!"
     takeInputsFromUser dictTrie
-  else if action == "p" then do
 
+  else if action == "p" then do
+    -- If action is "print", print all dictionary values
     putStrLn "Words are "
     let allWords = concat $ L.intersperse "\n" (reverse $ L.nub $ getWords dictTrie)
     putStrLn allWords
@@ -94,7 +100,7 @@ takeInputsFromUser dictTrie = do
   else if action == "e" then
     return ()
   else
-    error "Wrong Input"
+    error "Wrong Input."
 
 
 main = do
